dc8de1a29a0fbdbbff701c567e95e12ebbf22443
Carlo <carlo@hyperstacksinc.com>
2020-03-30 18:18:51 +0800

    last update

--- src/Code.js
+++ src/Code.js
@@ -42,19 +42,35 @@ function testing() {
     return true;
 }
 
-function useTemplate(id, newTitle) {
+function testFolder() {
+    var folderId = "13eVpv_8NJSWQIVETx0FgTa48oBl10doJ";
+    var exist = true;
+    try{
+        var testFolder = DriveApp.getFolderById(folderId);
+        Logger.log("success");
+    }
+    catch(err){
+        exist=false
+        Logger.log("fail");
+    }
+    return exist;
+}
+
+function useTemplate() {
+    var docId = "1Ozf6U16RXJV8Jn83mTzVEA_suWJnpZ9j9EqdrgWelbw";
+    var newTitle = "copy-of-Test";
+    var folderId = "13eVpv_8NJSWQIVETx0FgTa48oBl10doJ";
+    var owner = "carlo@hyperstacksinc.com";
     Logger.log(newTitle);
-    Logger.log(id);
-    // if(!id)
-    //     id = "1Ozf6U16RXJV8Jn83mTzVEA_suWJnpZ9j9EqdrgWelbw";
+    Logger.log(docId);
+    Logger.log(folderId);
+    Logger.log(owner);
     
-    var file = DriveApp.getFileById(id);
-    Logger.log(file);
-    var newfile = file.makeCopy(newTitle);
-    Logger.log(newfile);
-    // var doc = DocumentApp.openById(id);
-    // doc.getBody();
-    return true;    
+    var file = DriveApp.getFileById(docId);
+    var folder = DriveApp.getFolderById(folderId);
+    var newfile = file.makeCopy(newTitle, folder);
+    newfile.setOwner(owner);
+    Logger.log(newfile.getName());
 }
 
   
